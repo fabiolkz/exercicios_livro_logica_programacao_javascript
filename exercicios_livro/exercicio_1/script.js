9350d2706e05f16123b1dfcb42d6d23a7fba5add
fabiolkz <fabiodosantoslk@gmail.com>
2022-11-24 23:21:21 -0300

    Primeiro exercicio feito!

---
+++ exercicios_livro/exercicio_1/script.js
@@ -0,0 +1,6 @@
+
+let numero = Number(prompt('Informe um número valido: '));
+let numeroAnterior = numero - 1;
+let numeroPosterior = numero + 1;
+
+alert(`Vizinho: ${numeroAnterior} e ${numeroPosterior}`);
