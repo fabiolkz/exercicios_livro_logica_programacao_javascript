4bc160f98596d6b78b501ad9c0b3170536450e59
fabiolkz <fabiodosantoslk@gmail.com>
2023-01-01 21:16:34 -0300

    Exercicio criação de Chinchilas feito

--- exercicios_livro/capitulo_4_repetição/criacao_chinchilas/script.js
+++ exercicios_livro/capitulo_4_repetição/criacao_chinchilas/script.js
@@ -6,14 +6,29 @@ const verificaChichilas = () => {
     let chichilas = inChinchilas.value;
     let anos = Number(inAnos.value);
 
-    let totalChichilas = chichilas * 3;
+    let procriacaoChinchilas = `1º Ano: ${chichilas} Chinchilas` + `</br>`
 
-    for (i = 0; i < anos; i++) {
 
+    if (chichilas < 2 || (chichilas % 2) != 0) {
+        alert('Por favor insira um número maior que 2 para conseguirmos fazer a procriação!');
+        inChinchilas.focus();
+        inChinchilas.value = " ";
+        inAnos.value = " ";
+        return;
+    } else if (anos <= 1 || isNaN(anos)) {
+        alert('Insira pelo menos 2 anos para obter o tempo minimo de procriação das espécies!');
+        inAnos.focus();
+        inAnos.value = " ";
+        return;
     }
 
-    outResposta.textContent = totalChichilas;
+    for (i = 2; i <= anos; i++) {
+        chichilas = chichilas * 3;
+        procriacaoChinchilas += `${i}º Ano: ${chichilas} Chinchilas` + `</br>`
+        outResposta.innerHTML = procriacaoChinchilas;
+    }
 }
+
 
 let btCalc = document.querySelector('#btCalc');
 btCalc.addEventListener('click', verificaChichilas);
